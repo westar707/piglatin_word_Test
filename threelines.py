a6f2212791feebd0f65dea3405b9e8043a1853e8
Amanda West <awest@electronics-lab>
2020-04-28 11:11:29 -0500

    edit lines

--- threelines.py
+++ threelines.py
@@ -4,12 +4,10 @@ lines = [
     'this is another line',
     'this is a last line'
 ]
-
+f = open ('threelines.txt', 'w')
+f.write('add new line')
+f.close()
 f = open ('threelines.txt', 'r')
 lines = f.read()
 print(lines)
 f.close()
-f = open ('threelines.txt', 'w')
-f.write('add new line')
-f.close()
-
