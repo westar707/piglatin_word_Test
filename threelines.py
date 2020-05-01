a8de6e024dbf7705df686f1a6f8113b0d24fd325
Amanda West <awest@electronics-lab>
2020-05-01 13:23:02 -0500

    bug fix

--- threelines.py
+++ threelines.py
@@ -1,13 +1,18 @@
 
-lines = [
+mylines = [
     'this is a line',
     'this is another line',
     'this is a last line'
 ]
+for x in mylines:
+    print(x)
 f = open ('threelines.txt', 'w')
-f.write('add new line')
+f.write(x)
 f.close()
 f = open ('threelines.txt', 'r')
 lines = f.read()
-print(lines)
+print(x)
 f.close()
+with open('threelines.txt', 'w') as f:
+    for items in x:
+        f.write("%s\n" % items)
