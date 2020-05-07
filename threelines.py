2e82639e0d181f550f29182fc4c518142116d34f
Amanda West <awest@electronics-lab>
2020-05-07 11:48:02 -0500

    merged changes

--- threelines.py
+++ threelines.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 mylines = [
-=======
-lines = [
->>>>>>> d9e5289b8b9bd9b65f0db4e80acac834f8f290e4
     'this is a line',
     'this is another line',
     'this is a last line'
@@ -15,13 +11,8 @@ f.write(x)
 f.close()
 f = open ('threelines.txt', 'r')
 lines = f.read()
-<<<<<<< HEAD
 print(x)
 f.close()
 with open('threelines.txt', 'w') as f:
     for items in x:
         f.write("%s\n" % items)
-=======
-print(lines)
-f.close()
->>>>>>> d9e5289b8b9bd9b65f0db4e80acac834f8f290e4
